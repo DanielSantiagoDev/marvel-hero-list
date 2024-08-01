354d9e9ce23f0edf8cbad890902e45dbcf722f32
Daniel <Daniel Santiago>
2024-08-02 01:51:01 +0200

    timeout on pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {
     MARVEL_PUBLIC_KEY: process.env.MARVEL_PUBLIC_KEY,
     MARVEL_PRIVATE_KEY: process.env.MARVEL_PRIVATE_KEY,
   },
+  staticPageGenerationTimeout: 150000
 };
 
 export default nextConfig;
